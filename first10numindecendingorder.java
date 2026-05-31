04234444a91ba5259a17f5934a53980a34b57a99
Divy-the-chess-GOD <aadarsh0100@gmail.com>
2026-05-31 21:22:10 +0530

    Create first10numindecendingorder.java

---
+++ first10numindecendingorder.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("First 10 natural numbers in decreasing order:");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
